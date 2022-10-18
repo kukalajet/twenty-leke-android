84e1e6a9478ffbbe82a7a004ed94d6e7e73e97c3
Jeton Kukalaj <jeton.kukalaj@gmail.com>
2022-10-18 16:13:43 +0200

    add ui system components

---
+++ app/src/main/java/com/jeton/twentyleke/core/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.jeton.twentyleke.core.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val TwentyLekeShapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(8.dp)
+)
