40b251690200e40d7aa50fe89b1bc6e30355867c
Jeton Kukalaj <jeton.kukalaj@gmail.com>
2022-10-18 16:13:45 +0200

    Create HomeScreen.kt

---
+++ app/src/main/java/com/jeton/twentyleke/feature/home/view/HomeScreen.kt
@@ -0,0 +1,15 @@
+package com.jeton.twentyleke.feature.home.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen() {
+
+}
+
+@Preview
+@Composable
+fun PreviewHomeScreen() {
+    HomeScreen()
+}
