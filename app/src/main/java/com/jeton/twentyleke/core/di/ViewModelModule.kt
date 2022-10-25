794e588cdee945a1d45cf3cadff2682d2bb1ee2e
Jeton Kukalaj <jeton.kukalaj@gmail.com>
2022-10-25 08:54:00 +0200

    add `DetailViewModel`

--- app/src/main/java/com/jeton/twentyleke/core/di/ViewModelModule.kt
+++ app/src/main/java/com/jeton/twentyleke/core/di/ViewModelModule.kt
@@ -1,12 +1,12 @@
 package com.jeton.twentyleke.core.di
 
-import com.jeton.twentyleke.MainViewModel
 import com.jeton.twentyleke.core.repository.InvoiceCheckRepository
+import com.jeton.twentyleke.feature.detail.viewmodel.DetailViewModel
 import com.jeton.twentyleke.feature.scan.viewmodel.ScanViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { return@viewModel MainViewModel(get<InvoiceCheckRepository>()) }
     viewModel { return@viewModel ScanViewModel(get<InvoiceCheckRepository>()) }
+    viewModel { return@viewModel DetailViewModel(get<InvoiceCheckRepository>()) }
 }
---
+++ app/src/main/java/com/jeton/twentyleke/feature/detail/viewmodel/DetailViewModel.kt
@@ -0,0 +1,10 @@
+package com.jeton.twentyleke.feature.detail.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.jeton.twentyleke.core.repository.InvoiceCheckRepository
+
+class DetailViewModel(
+    private val invoiceCheckRepository: InvoiceCheckRepository
+) : ViewModel() {
+
+}
