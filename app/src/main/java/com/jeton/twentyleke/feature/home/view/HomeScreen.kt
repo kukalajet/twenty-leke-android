44cfd8452714ef96f1c139e5e88aada274ebfb65
Jeton Kukalaj <jeton.kukalaj@gmail.com>
2023-01-12 15:47:02 +0100

    add: refactoring in `HomeScreen`

--- app/src/main/java/com/jeton/twentyleke/feature/home/view/HomeScreen.kt
+++ app/src/main/java/com/jeton/twentyleke/feature/home/view/HomeScreen.kt
@@ -1,36 +1,54 @@
 package com.jeton.twentyleke.feature.home.view
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     navigateToScan: () -> Unit
 ) {
-    Scaffold(floatingActionButtonPosition = FabPosition.End, floatingActionButton = {
-        FloatingActionButton(
-            onClick = { navigateToScan() },
-            backgroundColor = MaterialTheme.colorScheme.primary,
-        ) {
-            Icon(
-                Icons.Filled.Add,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onPrimary,
+    Scaffold(
+        floatingActionButtonPosition = FabPosition.Center,
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                modifier = Modifier.padding(horizontal = 40.dp),
+                onClick = { navigateToScan() },
+                shape = FloatingActionButtonDefaults.extendedFabShape,
+                containerColor = MaterialTheme.colorScheme.primary,
             )
-        }
-    }) { innerPadding ->
+            {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        Icons.Filled.Add,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onPrimary,
+                        modifier = Modifier.padding(end = 4.dp)
+                    )
+                    Text(
+                        text = "Add new invoice",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onPrimary,
+                    )
+                }
+            }
+        },
+    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .padding(innerPadding)
